7aaf8848a0ccfc3343c8c163b7c25610d41ad433
Santiago Gomez <santiago.daniel.gomez@gmail.com>
2023-06-05 15:09:15 -0300

    first commit

---
+++ src/config/configServer.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const url = 'mongodb+srv://santiagodanielgomez:AhMbwxyx24izrjse@e-commerce.wnmhhjz.mongodb.net/?retryWrites=true&w=majority'
+
+
+const connectToDB = () => {
+    try {
+        mongoose.connect(url)
+        console.log('connected to DB e-commerce')
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default connectToDB
+
